0e4db10a3a1881716e8f86c4f539f63f5a9e1e34
qtose <65272194+qtose@users.noreply.github.com>
2021-01-18 23:42:22 +0900

    20210118_내적.cpp

---
+++ 20210118_내적.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//https://programmers.co.kr/learn/courses/30/lessons/70128
+
+int solution(vector<int> a, vector<int> b) {
+	int answer = 0;
+
+	for (size_t i = 0; i < a.size(); ++i)
+	{
+		answer += a[i] * b[i];
+	}
+
+	return answer;
+}
+
+int main() {
+
+	vector<int> a = { 1, 2, 3, 4 };
+	vector<int> b = { -3, -1, 0, 2 };
+
+	solution(a, b);
+
+	return 0;
+}
